9f01f59f7d6a4f2524b747c4b09c3cf92ae7d24b
Shawn <shawntaylor@weecology.org>
2015-10-11 21:19:37 -0400

    reading and processing data files

--- fireHistory/fireHistory.R
+++ fireHistory/fireHistory.R
@@ -3,10 +3,35 @@ libary(plyr)
 library(raster)
 setwd('~/projects/classProjects/fireHistory')
 
-site1=read.csv('iwSites.csv')
+
+fireData=data.frame(siteName=character(),
+                    frequency=integer(),
+                    firstYear=integer(),
+                    lastYear=integer())
+
+dataFiles=c('iwSites.csv','ncSites.csv','pnwSites.csv','swSites.csv')
+for(thisFile in dataFiles){
+  site=read.csv(thisFile)
+  trees=colnames(site)[-1]
+  for(treeName in trees){
+    treeData=site[,c('Year',treeName)]
+    rownames(treeData)=NULL
+    colnames(tree)=c('Year','Fire')
+    tree=tree[!is.na(tree$Fire),]
+    tree=tree[tree$Fire>0,]
+    tree$Fire=tree$Fire-1
+    
+    freq=mean(tree$Fire)
+    high=max(tree$Year)
+    low=min(tree$Year)
+    treeData=data.frame(siteName=treeName, frequency=freq, firstYear=low, lastYear=high)
+    fireData=rbind(fireData, treeData)
+  }
+}
+
 sites=read.csv('sites.csv')
 sites$long=sites$long*-1
 #coordinates(sites)=~lat+long
 
-map('state', resolution=0, myborder=-2)
+map('state', resolution=0)
 points(x=sites$long, y=sites$lat, col='red')
