0fd7191bdfe699593b20c7e71cbc9ece91563667
Shawn <shawn@weecology.org>
2015-10-14 19:46:48 -0400

    nice graph of tree data range with fire dots

--- fireHistory/fireHistory.R
+++ fireHistory/fireHistory.R
@@ -1,14 +1,20 @@
 #SWS 5182 paleo climate proxies assignment
 libary(plyr)
-library(raster)
+library(ggplot2)
+#library(raster)
 setwd('~/projects/classProjects/fireHistory')
 
 
 fireData=data.frame(siteName=character(),
                     region=character(),
-                    frequency=integer(),
-                    firstYear=integer(),
-                    lastYear=integer())
+                    Fire=integer(),
+                    year=integer())
+
+fireDataYearReference=data.frame(siteName=character(),
+                                 region=character(),
+                                 firstYear=integer(),
+                                 lastYear=integer())
+
 
 dataFiles=c('iwSites.csv','ncSites.csv','pnwSites.csv','swSites.csv')
 for(thisFile in dataFiles){
@@ -23,13 +29,38 @@ for(thisFile in dataFiles){
     tree$Fire=tree$Fire-1
     
     region=strsplit(thisFile,'S')[[1]][1]
-    freq=mean(tree$Fire)
+    #freq=mean(tree$Fire)
     high=max(tree$Year)
     low=min(tree$Year)
-    treeData=data.frame(siteName=treeName, region=region, frequency=freq, firstYear=low, lastYear=high)
-    fireData=rbind(fireData, treeData)
+    #treeData=data.frame(siteName=treeName, region=region, frequency=freq, firstYear=low, lastYear=high)
+    treeYears=data.frame(siteName=treeName, region=region,firstYear=low, lastYear=high)
+    tree$region=region
+    tree$siteName=treeName
+    fireData=rbind(fireData, tree)
+    fireDataYearReference=rbind(fireDataYearReference, treeYears)
   }
 }
+#fireData=fireData[fireData$region=='iw',]
+#fireDataYearReference=fireDataYearReference[fireDataYearReference$region=='iw',]
+
+siteNameRef=1:length(unique(fireData$siteName))
+siteNameRef=data.frame(siteName=sort(unique(fireData$siteName)), siteNum=1:length(unique(fireData$siteName)))
+fireData=merge(fireData, siteNameRef, by='siteName')
+fireDataYearReference=merge(fireDataYearReference, siteNameRef, by='siteName')
+
+
+  ggplot()+
+  theme_bw()+
+  geom_segment(data=fireDataYearReference, mapping=aes(x=firstYear, y=siteNum, xend=lastYear, yend=siteNum))+
+  geom_point(data=fireData[fireData$Fire==1,], mapping=aes(x=Year, y=siteNum),size=3, colour='red')+
+  xlab('Year')+
+  ylab('Trees')+
+  theme(axis.text.y=element_blank(), text=element_text(size=50))
+    
+
+  
+##############################
+  #plot all the sites on a map
 
 sites=read.csv('sites.csv')
 sites$long=sites$long*-1
