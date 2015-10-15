d3a86c6c6907b91df495cf39ea0e7ad84ca88358
Shawn <shawn@weecology.org>
2015-10-14 20:34:49 -0400

    10 year moving average analysis

--- fireHistory/fireHistory.R
+++ fireHistory/fireHistory.R
@@ -1,5 +1,5 @@
 #SWS 5182 paleo climate proxies assignment
-libary(plyr)
+library(plyr)
 library(ggplot2)
 #library(raster)
 setwd('~/projects/classProjects/fireHistory')
@@ -44,11 +44,12 @@ for(thisFile in dataFiles){
 #fireDataYearReference=fireDataYearReference[fireDataYearReference$region=='iw',]
 
 siteNameRef=1:length(unique(fireData$siteName))
-siteNameRef=data.frame(siteName=sort(unique(fireData$siteName)), siteNum=1:length(unique(fireData$siteName)))
+siteNameRef=data.frame(siteName=sort(unique(fireData$siteName)), siteNum=c(1:length(unique(fireData$siteName))+5))
 fireData=merge(fireData, siteNameRef, by='siteName')
 fireDataYearReference=merge(fireDataYearReference, siteNameRef, by='siteName')
 
-
+####################################
+#Tree data range graph with dots fore fires
   ggplot()+
   theme_bw()+
   geom_segment(data=fireDataYearReference, mapping=aes(x=firstYear, y=siteNum, xend=lastYear, yend=siteNum))+
@@ -56,7 +57,42 @@ fireDataYearReference=merge(fireDataYearReference, siteNameRef, by='siteName')
   xlab('Year')+
   ylab('Trees')+
   theme(axis.text.y=element_blank(), text=element_text(size=50))
-    
+
+####################################    
+#Analysis
+
+freqData=data.frame(period=integer(),
+                    siteName=character(),
+                    freq=integer(),
+                    region=character())
+
+beg=min(fireData$Year)
+end=max(fireData$Year)
+step=10
+thisStep=beg
+while(thisStep<end){
+  thisStepData=fireData[fireData$Year>thisStep & fireData$Year<thisStep+10,]
+  thisStepFreq=ddply(thisStepData, c('siteName','region'), summarize, freq=mean(Fire))
+  thisStepFreq$period=thisStep
+  freqData=rbind(freqData, thisStepFreq)
+  thisStep=thisStep+step
+}
+
+freqData=ddply(freqData, c('region','period'), summarize, freq=mean(freq))
+
+for(thisRegion in unique(freqData$region)){
+  print(with(freqData[freqData$region==thisRegion,], plot(freq~period, type='l',main=thisRegion, ylab='Fire Frequency',xlab='Year', col='red',lwd=9.5, ylim=c(0,1), xlim=c(1100, 2000))))
+}
+
+
+ggplot(freqData, aes(x=period, y=freq, colour=region, group=region))+geom_line(size=3)+
+  theme_bw()+
+  xlab('Year')+
+  ylab('Fire Frequency')+
+  theme(legend.position=c(0.8,0.8))+
+  theme(text=element_text(size=50))+
+  theme(legend.text=element_text(size=50))+
+  theme(legend.position=c(0.8,0.8))
 
   
 ##############################
